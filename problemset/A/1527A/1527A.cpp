e0ed0b50c1f643c2cd49f10ab6a68560e82175a3
Jack Tang <tanghocle456@gmail.com>
2021-10-17 17:11:13 -0700

    Update Gitignore and Sol
    As titled

---
+++ problemset/A/1527A/1527A.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+
+    int64_t n =0;
+    cin >> n;
+
+    int64_t i = 0;
+    int64_t x = 0;
+    int64_t s = 0;
+
+    while(n--) {
+        cin >> x;
+        int32_t count = 0;
+
+        while(x) {
+            count++;
+            x/=2;
+        }
+
+        cout << (1 << count-1) - 1 << endl;
+    }
+
+    return 0;
+}
