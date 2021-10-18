ea75a6d64a1d1b11d4ee8f4fb3d2081258a03465
Jack Tang <tanghocle456@gmail.com>
2021-10-17 21:04:06 -0700

    Update sol cpp

---
+++ problemset/C/4C/4C.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <map>
+#include <string>
+using namespace std;
+
+int main() {
+    int64_t x =0;
+    cin >> x;
+    cin.ignore();
+    map<string, int> mp;
+    string str;
+    while(x--) {
+        getline(cin,str);
+        if(mp[str]==0) {
+            cout << "OK\n";
+        } else {
+            cout << str << mp[str] << '\n';
+        }
+        mp[str]++;
+    }
+
+    return 0;
+}
