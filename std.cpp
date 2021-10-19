5840cb24da972762769ee420e98fa55025663f43
Jack Tang <tanghocle456@gmail.com>
2021-10-18 18:03:25 -0700

    Add standard template
    Added standard c++ template to use for competitive programming

---
+++ std.cpp
@@ -0,0 +1,27 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+#define MODU 1000000007
+#define EPS 1e-9 
+#define PI 3.1415926535897932384626433832795
+
+#define i(t) cin >> t
+#define il(t) getline(cin,t)
+
+#define o(t) cout << t << '\n'
+
+typedef vector<int> VI;
+typedef vector<vector<int>> VVI;
+typedef vector<pair<int,int>> VPII;
+
+typedef vector<string> VS, VSTR;
+
+typedef map<int, int> MII;
+typedef map<char, int> MCI;
+
+int main() {
+
+    
+    return 0;
+}
