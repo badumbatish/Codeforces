5f560fa76c5cf6a78d66de3a4e64f7011c222686
Jack Tang <tanghocle456@gmail.com>
2021-10-26 23:00:15 -0700

    Redone graph implementation
    Changing it to unorderd_map instead of vector

--- graphs.md
+++ graphs.md
@@ -3,26 +3,7 @@
 ### Let's talk graph problems and their implementation in C++
 
 ## DFS
-
-The Depth First Search introduced here will be implemented in a 
-recursive fashion
-
-We will traverse the graph recursively, pre-orderly
-Pseudo Code:
-```
-n = number of nodes in the graph
-g = adjacency list representing the graph
-
-array visited = [false, false, false, ...., false] # size n
-
-function dfs(at):
-    if visited[at]: return
-    visited[at] = true
-
-    for neighbour in neighbours of graph[a]:
-        dfs(next)
-
-start_node = 0
-dfs(start_node)
-
-```
+Please take a look inside dfs.cpp for the implementation of the following
+-  Depth First Search
+-  Connected Component
+-  Strongly Connected Component
--- graphs/dfs.cpp
+++ graphs/dfs.cpp
@@ -1,33 +1,70 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+
 using namespace std;
 
-unordered_map<char, bool> visited;
+typedef unordered_map<int, vector<int>> Graph;
 
-template<typename T> void dfs(T current, vector<vector<T>> &v) {
-    if(visited[current]) return;
-    visited[current] = true;
+unordered_map<int, bool> visited;
+Graph graph;
+
+
+// TESTED
+template<typename T> void dfs(T root) {
+    if(visited[root]) return;
+    visited[root] = true;
     //
     // Put your action on each node you traverse here.
-    // cout << current << endl;
+    cout << root << endl;
 
-    for(auto neighbour : v[current] ) {
-        dfs(neighbour,v);
+    for(auto neighbour : graph[root] ) {
+        dfs(neighbour);
     }
+}
+
+
+// TESTED
+// Connected component is for un directed graphs
+template<typename T> int count_connected_component(unordered_map<T, vector<T>> graph) {
+    int counter = 0;
+    
+    for(auto node : graph) {
+        if(!visited[node.first]) {
+            counter++;
+            cout << node.first << endl;
+            dfs(node.first);
+        }
+    }
+
+    return counter;
 }
 
 
 
 int main() {
     int start_node = 0;
-    vector<vector<int>> v(6);
-    v[0].push_back(1);
-    v[0].push_back(4);
-    v[1].push_back(-1);
-    v[1].push_back(3);
-    v[1].push_back(3);
-    dfs(0,v);
+
+    graph[0].push_back(1);
+    graph[1].push_back(0);
+
+    graph[0].push_back(4);
+    graph[4].push_back(0);
+
+    graph[1].push_back(-1);
+    graph[-1].push_back(1);
+    
+    graph[1].push_back(3);
+    graph[1].push_back(3);
+    graph[3].push_back(1);
+
+    graph[-1].push_back(2);
+    graph[2].push_back(-1);
+    
+    //dfs(0);
+    
+
+    cout << count_connected_component(graph);
 
     return 0;
 }
